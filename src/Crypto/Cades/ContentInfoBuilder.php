666e649d3127a685a7749a4e199584a93c3676ef
leonardo chaves <leonardoaschaves@gmail.com>
2026-05-18 20:01:26 -0300

    feat: build CMS ContentInfo envelope

---
+++ src/Crypto/Cades/ContentInfoBuilder.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NihilLabs\Pades\Crypto\Cades;
+
+use NihilLabs\Pades\Crypto\Asn1\Der;
+
+final readonly class ContentInfoBuilder
+{
+    public function build(string $signedData): string
+    {
+        return Der::sequence(
+            Der::oid('2a864886f70d010702')
+            . Der::contextSpecificConstructed(
+                0,
+                $signedData
+            )
+        );
+    }
+}
---
+++ tests/ContentInfoBuilderTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NihilLabs\Pades\Tests;
+
+use NihilLabs\Pades\Certificate\PfxCertificate;
+use NihilLabs\Pades\Crypto\Cades\ContentInfoBuilder;
+use NihilLabs\Pades\Crypto\Cades\SignedAttributesBuilder;
+use NihilLabs\Pades\Crypto\Cades\SignedAttributesSigner;
+use NihilLabs\Pades\Crypto\Cades\SignedDataBuilder;
+use NihilLabs\Pades\Crypto\Cades\SignerInfoBuilder;
+use PHPUnit\Framework\TestCase;
+
+final class ContentInfoBuilderTest extends TestCase
+{
+    public function test_it_builds_content_info(): void
+    {
+        $certificate = new PfxCertificate(
+            path: __DIR__ . '/Fixtures/certificate.pfx',
+            password: '123456'
+        );
+
+        $attributes = (new SignedAttributesBuilder())
+            ->build(
+                data: 'hello world',
+                certificatePem: $certificate->getPublicCertificate()
+            );
+
+        $signature = (new SignedAttributesSigner($certificate))
+            ->sign($attributes);
+
+        $signerInfo = (new SignerInfoBuilder())
+            ->build(
+                certificate: $certificate,
+                signedAttributes: $attributes,
+                encryptedDigest: $signature
+            );
+
+        $signedData = (new SignedDataBuilder())
+            ->build(
+                certificate: $certificate,
+                signerInfo: $signerInfo
+            );
+
+        $contentInfo = (new ContentInfoBuilder())
+            ->build($signedData);
+
+        $this->assertNotEmpty($contentInfo);
+
+        $this->assertStringStartsWith(
+            "\x30",
+            $contentInfo
+        );
+
+        $this->assertStringContainsString(
+            hex2bin('2a864886f70d010702'),
+            $contentInfo
+        );
+    }
+}
